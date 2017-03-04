24d45a1193d0ba2a99c373b1b55da28bc318133c
Phil <Philip.Shaughnessy@gmail.com>
2017-03-04 18:05:47 -0500

    added lots of puts in follow_toggle

--- app/controllers/socialize_controller.rb
+++ app/controllers/socialize_controller.rb
@@ -3,12 +3,18 @@ class SocializeController < ApplicationController
   before_action :require_user, only: [:follow_toggle]
 
   def follow_toggle
+    puts "trying to follow ......"
     user = User.find_by(username: params[:username])
+    puts "checked, user is #{user.username}"
+    puts "params are:"
+    p params
+    puts "end params ........"
     if user
       puts "followed!"
       current_user.toggle_follow!(user)
       render json: "follow status changed!"
     else
+      puts "sending not a user :( ........"
       request_error("not a user.")
     end
 
