37ba5ed0586959f70dc6943238f82c99a7046b8b
Phil <Philip.Shaughnessy@gmail.com>
2017-03-04 19:09:57 -0500

    fixed json in follow

--- app/controllers/socialize_controller.rb
+++ app/controllers/socialize_controller.rb
@@ -3,19 +3,12 @@ class SocializeController < ApplicationController
   before_action :require_user, only: [:follow_toggle]
 
   def follow_toggle
-    puts "trying to follow ......"
     user = User.find_by(username: params[:username])
-    puts "checked, user is #{user.username}" if user
-    puts "params are:"
-    p params
-    puts "end params ........"
     if user
-      puts "followed!"
       current_user.toggle_follow!(user)
-      message = "follow status changed!"
+      message = {message: "follow status changed!"}
       render json: message
     else
-      puts "sending not a user :( ........"
       request_error("not a user.")
     end
 
